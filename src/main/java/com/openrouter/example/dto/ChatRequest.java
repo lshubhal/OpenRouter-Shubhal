091469cfad19fc2ee5d4bb5e3adb2e09defd3cdd
Shubham Lokhande <shubhal.lokhande@gmail.com>
2025-08-05 22:53:47 +0530

    first commit

---
+++ src/main/java/com/openrouter/example/dto/ChatRequest.java
@@ -0,0 +1,13 @@
+package com.openrouter.example.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class ChatRequest {
+    private String chatId;
+    private String message;
+}
